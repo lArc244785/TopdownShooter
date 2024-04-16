2dd14b2a4d5cff0df00deeb9531740db0bed427e
lArc244785 <63329185+lArc244785@users.noreply.github.com>
2024-04-17 03:24:41 +0900

    fix: state machine

--- Assets/01.Scripts/FSM/StateMachineOfT.cs
+++ Assets/01.Scripts/FSM/StateMachineOfT.cs
@@ -9,35 +9,34 @@ namespace TopdownShooter.FSM
 {
 	public abstract class StateMachine<T> where T : Enum
 	{
-		private T _currentID;
-		public T currentID => _currentID;
+		public T currentID { get; private set; }
 
 		private Dictionary<T, IState<T>> _states;
 
 		public virtual void Init(IDictionary<T, IState<T>> setting)
 		{
 			_states = new Dictionary<T, IState<T>>(setting);
-			_currentID = _states.First().Key;
-			_states[_currentID].OnStateEnter();
+			currentID = _states.First().Key;
+			_states[currentID].OnStateEnter();
 		}
 
 		public bool ChangeState(T newState)
 		{
-			if (Comparer<T>.Default.Compare(_currentID, newState) == 0)
+			if (Comparer<T>.Default.Compare(currentID, newState) == 0)
 				return false;
 
 			if (!_states[newState].canExecute)
 				return false;
 
-			_states[_currentID].OnStateExit();
-			_currentID = newState;
-			_states[_currentID].OnStateEnter();
+			_states[currentID].OnStateExit();
+			currentID = newState;
+			_states[currentID].OnStateEnter();
 			return true;
 		}
 
 		public void OnStateUpdate()
 		{
-			var nextID = _states[_currentID].OnStateUpdate();
+			var nextID = _states[currentID].OnStateUpdate();
 			ChangeState(nextID);
 		}
 
